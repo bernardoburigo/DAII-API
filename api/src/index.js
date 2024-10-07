79e663f1cf59ad52ac8661f3058f89a3ca4a552d
Bernardo Goulart Búrigo <bernardoburigo@hotmail.com>
2024-10-07 14:53:45 -0300

    atualização docódigo

--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,52 @@
-const app = require('express')();
-const PORT = 8080;
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const { v4: uuidv4 } = require('uuid');
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
-app.listen(PORT, () => console.log(`Rodando em http://localhost:${PORT}`))
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+
+const options = {
+
+	definition: {
+
+		openapi: "3.0.0",
+
+		info: {
+			title: "DAII-API",
+			version: "1.0.0",
+			description: `API para o trabalho 1 de Desenvolvimento de Aplicações II 2024.
+                        Equipe: Bernardo Goulart Búrigo`,
+
+            license: {
+            name: 'Desenvolvimento de Aplicações II',
+            },
+
+            contact: {
+            name: 'Bernardo Goulart Búrigo'
+            },
+
+	    },
+
+		servers: [{
+			url: "http://localhost:8080/",
+            description: 'Development server',
+		},],
+
+	},
+
+	apis: ["./src/routes/*.js"],
+
+};
+const specs = swaggerJsDoc(options);
+
+app.use('/', routes)
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Rodando em http://localhost:${PORT}`));
---
+++ api/src/routes/indexRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+const appointment = require('./appointRoutes');
+const event = require('./eventsRoutes');
+const professional = require('./professionalsRoutes');
+const student = require('/studentsRoutes');
+const teacher = require('./teachersRoutes');
+const user = require('./usersRoutes');
+
+router.use(express.json());
+router.use('/appointments', appointment );
+router.use('/events', event);
+router.use('/professionals', professional);
+router.use('/students', student);
+router.use('/teachers', teacher);
+router.use('/users', user);
+
+module.exports = router;
